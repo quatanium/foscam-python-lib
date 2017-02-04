a290d84733315c706373a7c7a0b397341ef80bcd
sinchb <sinchb128@gmail.com>
2017-02-04 18:35:28 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -4,17 +4,36 @@ from setuptools import setup, find_packages
 import os
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Customer Service',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Telecommunications Industry',
+    'Natural Language :: English',
+    'Operating System :: POSIX',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Multimedia :: Sound/Audio',
+    'Topic :: Utilities',
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+]
 
 setup(
-    name='foscam',
-    version='1.0',
-    description='Hashicorp vault backend for python-keyring',
-    long_description=read('README.md'),
-    author='Philipp Schmitt',
-    author_email='philipp.schmitt@post.lu',
+    name='pyfoscam',
+    version='1.2',
+    description='Foscam Python Library for H.264 IP Cameras (FI9821W/P/HD816W/P)',
+    long_description=open('README.rst', 'r').read(),
+    author='Cherish Chen',
+    author_email='sinchb128@gmail.com',
     url='https://github.com/quatanium/foscam-python-lib',
-    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    keywords=['foscam', 'Camera', 'IPC'],
 )
